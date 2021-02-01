6d7a87fd7dc710902edc8fd0431e3e66ad975409
anonymous <anonymous>
2021-02-01 08:23:34 +0100

    SURAPP-51: Add tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
