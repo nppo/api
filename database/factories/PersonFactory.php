647aadf6e39e20374c2737b7b68be36f61510143
anonymous <anonymous>
2021-02-09 17:02:33 +0100

    SURAPP-50: Removes deleted field from factory.

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -13,11 +13,8 @@ class PersonFactory extends Factory
 
     public function definition(): array
     {
-        $prefixes = ['de', 'van', 'van der'];
-
         return [
             'first_name'       => $this->faker->firstName,
-            'last_name_prefix' => $this->faker->optional(0.1)->randomElement($prefixes),
             'last_name'        => $this->faker->lastName,
             'email'            => $this->faker->email,
             'function'         => $this->faker->jobTitle,
