5cb3414d7bfaa5a4066b06bf230b646baf49aa10
anonymous <anonymous>
2021-02-09 16:59:27 +0100

    SURAPP-50: Adds uncommitted file in addition to previous commit...

--- database/migrations/2021_02_09_150210_create_contributables_table.php
+++ database/migrations/2021_02_09_150210_create_contributables_table.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonProductTable extends Migration
+class CreateContributablesTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('person_product', function (Blueprint $table): void {
-            $table->foreignId('person_id')->constrained()->onDelete('cascade');
+        Schema::create('contributables', function (Blueprint $table): void {
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('contributable_id');
+            $table->string('contributable_type');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('person_product');
+        Schema::dropIfExists('contributables');
     }
 }
