ddb6d8f05624709b9ada1cd316852cf37bf5289b
anonymous <anonymous>
2021-02-10 10:15:37 +0100

    SURAPP-177: Add stage URL

--- database/seeders/PassportSeeder.php
+++ database/seeders/PassportSeeder.php
@@ -14,7 +14,7 @@ class PassportSeeder extends Seeder
         Client::create([
             'id'                     => '92aea21e-2f37-4095-b604-dd6b3fc16a5e',
             'name'                   => 'surapp_frontend_local',
-            'redirect'               => 'http://localhost:3000/login',
+            'redirect'               => 'http://localhost:3000/login,https://surapp.stage2web.nl/login',
             'personal_access_client' => false,
             'password_client'        => false,
             'revoked'                => false,
