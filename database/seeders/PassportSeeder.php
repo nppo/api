c788b366c69f9156fb9dadbae292770c660882dc
anonymous <anonymous>
2021-03-01 05:50:32 +0100

    Update passport seeder

--- database/seeders/PassportSeeder.php
+++ database/seeders/PassportSeeder.php
@@ -12,9 +12,13 @@ class PassportSeeder extends Seeder
     public function run(): void
     {
         Client::create([
-            'id'                     => '92aea21e-2f37-4095-b604-dd6b3fc16a5e',
-            'name'                   => 'surapp_frontend_local',
-            'redirect'               => 'http://localhost:3000/login,https://surapp.stage2web.nl/login',
+            'id'       => '92aea21e-2f37-4095-b604-dd6b3fc16a5e',
+            'name'     => 'surapp_frontend_local',
+            'redirect' => implode(',', [
+                'http://localhost:3000/login',
+                'https://surapp.stage2web.nl/login',
+                'https://surapp-web.accept2web.nl/login',
+            ]),
             'personal_access_client' => false,
             'password_client'        => false,
             'revoked'                => false,
