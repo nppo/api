41e4b308a5e57a3627cd78e861ad09d8732cb621
anonymous <anonymous>
2021-03-12 10:03:45 +0100

    SURAPP-248: Lint

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Laravel\Scout\Searchable;
 use Way2Web\Force\AbstractModel;
