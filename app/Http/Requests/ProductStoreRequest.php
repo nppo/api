18f247d62f9f9810aae8c7f0cc81df39facda753
anonymous <anonymous>
2021-03-23 13:03:37 +0100

    Fix merge

--- app/Http/Requests/ProductStoreRequest.php
+++ app/Http/Requests/ProductStoreRequest.php
@@ -38,50 +38,10 @@ class ProductStoreRequest extends FormRequest
         ];
     }
 
-declare(strict_types=1);
-
-namespace App\Http\Requests;
-
-use App\Enumerators\Mimes;
-use App\Enumerators\ProductTypes;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-
-class ProductStoreRequest extends FormRequest
-{
-    public function rules(): array
-    {
-        return [
-            'type' => [Rule::in(ProductTypes::asArray())],
-
-            'title'        => ['required'],
-            'description'  => ['nullable'],
-            'published_at' => ['nullable', 'date'],
-            'summary'      => ['nullable'],
-
-            'tags'         => ['array', 'nullable'],
-            'tags.*.label' => ['required', 'string'],
-
-            'themes'      => ['array', 'nullable'],
-            'themes.*.id' => ['required', 'integer'],
-
-            'people'      => ['array', 'nullable'],
-            'people.*.id' => ['required', 'integer'],
-
-            'parties'      => ['array', 'nullable'],
-            'parties.*.id' => ['required', 'integer'],
-
-            'link' => ['required_without:file', 'string', 'url'],
-            'file' => ['required_without:link', 'mimes:' . Mimes::asArrayString()],
-        ];
-    }
-
     protected function prepareForValidation(): void
     {
         $this->merge([
             'published_at' => $this->get('publishedAt'),
         ]);
     }
-}
-
 }
