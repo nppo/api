5f491e2daaf34f0118ac147da38b6b19da7eae4f
anonymous <anonymous>
2021-03-25 18:46:57 +0100

    SURAPP-264: Fixes linting

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -81,7 +81,7 @@ class ProjectRepository extends AbstractRepository
                 ->structureRepository
                 ->makeQuery()
                 ->with([
-                    'attributes'
+                    'attributes',
                 ])
                 ->where('label', $this->model())
                 ->first()
