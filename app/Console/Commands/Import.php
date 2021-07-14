9ceb801afa3aa9ba9944ac094e47f0b507bac677
anonymous <anonymous>
2021-07-14 15:35:12 +0200

    SURAPP-295: Adjust CLI interface and through that allow users to define which import they want to run

--- app/Console/Commands/Import.php
+++ app/Console/Commands/Import.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Import\Jobs\Import as ImportJob;
 use App\Import\Jobs\ImportAll;
 use Illuminate\Console\Command;
 
 class Import extends Command
 {
-    protected $signature = 'import';
+    protected $signature = 'import {driver?}';
 
     protected $description = 'Imports data from external systems';
 
     public function handle(): void
     {
+        if ($this->hasArgument('driver') && !empty($this->argument('driver'))) {
+            ImportJob::dispatch($this->argument('driver'));
+            return;
+        }
+
         ImportAll::dispatch();
     }
 }
--- app/Import/Jobs/Import.php
+++ app/Import/Jobs/Import.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Import\Jobs;
 
-use App\Enumerators\ImportDriver;
 use App\Enumerators\Queue;
 use App\Import\ConnectionResolver;
 use Illuminate\Bus\Queueable;
@@ -13,21 +12,18 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\App;
 
-class ImportAll implements ShouldQueue
+class Import implements ShouldQueue
 {
     use Queueable, InteractsWithQueue, Dispatchable;
 
-    public function __construct()
+    public function __construct(string $driver)
     {
+        $this->driver = $driver;
         $this->queue = Queue::IMPORT_EXTERNAL;
     }
 
     public function handle(): void
     {
-        $connectionResolver = App::make(ConnectionResolver::class);
-
-        foreach (ImportDriver::asArray() as $importDriver) {
-            $connectionResolver->resolve($importDriver)->import();
-        }
+        App::make(ConnectionResolver::class)->resolve($this->driver)->import();
     }
 }
