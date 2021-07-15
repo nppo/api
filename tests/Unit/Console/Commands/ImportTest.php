ebfff2413bf60c02504ef620925d3374084a82f6
anonymous <anonymous>
2021-07-15 06:36:01 +0200

    SURAPP-295: Add additional tests for the newly created funcionality

---
+++ tests/Unit/Console/Commands/ImportTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit\Console\Commands;
+
+use App\Enumerators\ImportDriver;
+use App\Import\Jobs\Import;
+use App\Import\Jobs\ImportAll;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Queue;
+use Tests\TestCase;
+
+class ImportTest extends TestCase
+{
+    /** @test */
+    public function it_will_queue_import_all_when_no_driver_is_specified(): void
+    {
+        Queue::fake();
+
+        Artisan::call('import');
+
+        Queue::assertPushed(ImportAll::class);
+    }
+
+    /** @test */
+    public function it_will_queue_a_import_when_a_driver_is_specified(): void
+    {
+        Queue::fake();
+
+        Artisan::call('import ' . ImportDriver::STRAPI);
+
+        Queue::assertPushed(Import::class);
+    }
+}
---
+++ tests/Unit/Import/Jobs/ImportTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Import;
+
+use App\Enumerators\Queue as EnumeratorsQueue;
+use App\Import\ConnectionResolver;
+use App\Import\Connections\Contracts\ImportConnection;
+use App\Import\Jobs\Import;
+use Illuminate\Support\Facades\Queue;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tests\TestCase;
+
+class ImportTest extends TestCase
+{
+    /** @test */
+    public function it_is_put_on_the_right_queue(): void
+    {
+        Queue::fake();
+
+        Import::dispatch('::DOES_NOT_EXIST::');
+
+        Queue::assertPushedOn(EnumeratorsQueue::IMPORT_EXTERNAL, Import::class);
+    }
+
+    /** @test */
+    public function handle_will_resolve_the_provided_driver(): void
+    {
+        /** @var ConnectionResolver|MockObject */
+        $connectionResolver = $this->createMock(ConnectionResolver::class);
+
+        $connectionResolver
+            ->expects($this->once())
+            ->method('resolve')
+            ->with('::DOES_NOT_EXIST::')
+            ->willReturn(
+                $this->createMock(ImportConnection::class)
+            );
+
+        (new Import('::DOES_NOT_EXIST::'))->handle($connectionResolver);
+    }
+}
