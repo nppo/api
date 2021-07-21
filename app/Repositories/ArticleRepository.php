124ab02de8b9c12f296b02a4d151eb8171fe87f3
anonymous <anonymous>
2021-07-21 14:12:33 +0200

    SURAPP-298: Add Articles to search results

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Enumerators\Filters;
 use App\Models\Article;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Way2Web\Force\Repository\AbstractRepository;
 
 class ArticleRepository extends AbstractRepository
 {
+    protected ?Builder $builder = null;
+
+    public function __construct()
+    {
+        $this->builder = $this->makeQuery();
+    }
+
     public function model(): string
     {
         return Article::class;
+    }
+
+    public function index(): Collection
+    {
+        return $this
+            ->makeQuery()
+            ->orderBy('title')
+            ->get();
     }
 
     public function show($id): Model
@@ -20,5 +38,44 @@ class ArticleRepository extends AbstractRepository
         return $this
             ->with(['tags', 'themes'])
             ->findOrFail($id);
+    }
+
+    public function search(string $query): self
+    {
+        $this
+            ->builder
+            ->with(['tags', 'themes'])
+            ->withCount('likes');
+
+        if ($query !== '') {
+            $this->builder->where('title', 'LIKE', "%{$query}%");
+        }
+
+        return $this;
+    }
+
+    public function filter(array $filters = []): self
+    {
+        foreach ($filters as $key => $value) {
+            if ($key === Filters::THEMES) {
+                $this->builder->whereHas('themes', function ($query) use ($value): void {
+                    $query->whereIn('id', $value);
+                });
+            }
+        }
+
+        return $this;
+    }
+
+    public function orderBy(string $column, string $order = 'asc'): self
+    {
+        $this->builder->orderBy($column, $order);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->builder->get();
     }
 }
