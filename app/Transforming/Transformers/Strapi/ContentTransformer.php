019a8e9242ccfe7cc2d2106f5d740a9d9ee29115
anonymous <anonymous>
2021-07-23 13:09:09 +0200

    Make adjustment to transformer

--- app/Transforming/Transformers/Strapi/ContentTransformer.php
+++ app/Transforming/Transformers/Strapi/ContentTransformer.php
@@ -8,19 +8,30 @@ use App\Transforming\Interfaces\Transformer;
 
 class ContentTransformer implements Transformer
 {
-    public function transform($value): array
+    /** @param mixed $value */
+    public function transform($value)
     {
-        return $this->decorate($value);
+        if (is_string($value)) {
+            return $this->decorate($value);
+        }
+
+        return $this->decorateSet($value);
     }
 
-    private function decorate(array $content): array
+    /** @param mixed $subContent */
+    private function decorate($subContent): string
+    {
+        return preg_replace('(\/[a-zA-Z]*\/.*)', config('strapi.url') . '$0', $subContent);
+    }
+
+    private function decorateSet(array $content): array
     {
         return array_map(function ($subContent) {
             if (is_array($subContent)) {
-                return $this->decorate($subContent);
+                return $this->decorateSet($subContent);
             }
 
-            return preg_replace('(\/[a-zA-Z]*\/.*)', config('strapi.url') . '$0', $subContent);
+            return $this->decorate($subContent);
         }, $content);
     }
 }
