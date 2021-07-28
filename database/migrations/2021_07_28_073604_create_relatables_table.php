60a594fd6eb825c6f09401b7a69538b28828663d
anonymous <anonymous>
2021-07-28 11:01:46 +0200

    SURAPP-312: Remove autofixed code

--- database/migrations/2021_07_28_073604_create_relatables_table.php
+++ database/migrations/2021_07_28_073604_create_relatables_table.php
@@ -15,28 +15,7 @@ class CreateRelatablesTable extends Migration
         });
     }
 
-    public function down()
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-class CreateRelatablesTable extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('relatables', function (Blueprint $table) {
-            $table->foreignId('article_id');
-
-            $table->uuidMorphs('relatable');
-        });
-    }
-
     public function down(): void
-    {
-        Schema::dropIfExists('relatables');
-    }
-}
-
     {
         Schema::dropIfExists('relatables');
     }
