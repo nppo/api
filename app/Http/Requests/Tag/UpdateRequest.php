ac9a95e2e6b08598d1df61128b348afc6e15f4cf
anonymous <anonymous>
2021-07-30 08:06:05 +0200

    SURAPP-288: Remove the ability to specify the type of a tag

--- app/Http/Requests/Tag/UpdateRequest.php
+++ app/Http/Requests/Tag/UpdateRequest.php
@@ -1,17 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Tag;
 
-use App\Enumerators\TagTypes;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class UpdateRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'type' => ['nullable', 'string', Rule::in(TagTypes::asArray())],
             'label' => ['required', 'string', 'min:3'],
         ];
     }
